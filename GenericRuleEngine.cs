ad87ca8600a799009090c209ce4846bb65dc5bd8
abdul khan <abdulvar.khan001@gmail.com>
2023-01-30 13:25:28 +0530

    Add files via upload

---
+++ GenericRuleEngine.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using YamlDotNet.RepresentationModel;
+using YamlDotNet.Helpers;
+using System.Collections.Generic;
+using System.Linq;
+
+class GenericRuleEngine
+{
+    private static List<YamlNode> GetValues(List<string> nodes, YamlNode node)
+    {
+        List<YamlNode> nodeToReturn = new List<YamlNode>();
+        var nodeslist = new List<string>(nodes);
+        foreach (string nodeitem in nodes)
+        {
+            //Console.WriteLine("\n"+node.NodeType);
+            //Console.WriteLine("\n" + nodeitem);
+            
+            if (node.NodeType.ToString() == "Sequence")
+            {
+                // ((YamlDotNet.RepresentationModel.YamlSequenceNode)node).Children
+                foreach (var item in ((YamlSequenceNode)node).Children)
+                {
+                    nodeToReturn.AddRange(GetValues(nodeslist, item));
+                }
+
+            }
+            else if (node.NodeType.ToString() == "Mapping" && ((YamlMappingNode)node).Children.ContainsKey(nodeitem))
+            {
+                node = node[(nodeitem)];
+            }
+            if (node.NodeType.ToString() == "Scalar")
+            {
+                nodeToReturn.Add(node);
+            }
+            if (node == null)
+            {
+                return null;
+            }
+
+        }
+        return nodeToReturn;
+    }
+    public static List<Tuple<bool, int, int>> ValidateRule(YamlNode rootNode, string tagName, string expectedType, string regex)
+    {
+        List<Tuple<bool, int, int>> results = new List<Tuple<bool, int, int>>();
+        //var node = rootNode.Children[new YamlScalarNode(tagName)];
+        var nodes = tagName.Split(".");
+        //var node = rootNode;
+        //var seqNode = new YamlDotNet.RepresentationModel.YamlSequenceNode().Children.Count;
+        //var mappingNode1 = new YamlDotNet.RepresentationModel.YamlMappingNode().Children.Count;
+        //var tscalerNode1 = new YamlDotNet.RepresentationModel.YamlScalarNode().Value;
+        List<string> listNodes = new List<string>(nodes);
+        List<YamlNode> resultvalues = GetValues(listNodes, rootNode);
+       // var value = node.ToString();
+        //var node = rootNode.AllNodes.[(tagName)];
+        //Console.WriteLine(value);
+        //var node = rootNode.Children[new YamlScalarNode(tagName)];
+        foreach (var node in resultvalues.Distinct())
+        {
+            //if (node.Tag  == tagName)
+            {
+                var value = node.ToString();
+                Console.WriteLine("values: "+value);
+                if (node != null)
+                {
+                    //var value = node.ToString();
+                    if (expectedType == "int")
+                    {
+                        int result;
+                        if (!int.TryParse(value, out result))
+                        {
+                            results.Add(Tuple.Create(false, node.Start.Line, node.End.Line));
+                        }
+                        else
+                        {
+                            results.Add(Tuple.Create(true, node.Start.Line, node.End.Line));
+                        }
+                    }
+                    else if (expectedType == "string")
+                    {
+                        if (!string.IsNullOrEmpty(regex))
+                        {
+                            if (!Regex.IsMatch(value, regex))
+                            {
+                                results.Add(Tuple.Create(false, node.Start.Line, node.End.Line));
+                            }
+                            else
+                            {
+                                results.Add(Tuple.Create(true, node.Start.Line, node.End.Line));
+                            }
+                        }
+                        else
+                        {
+                            results.Add(Tuple.Create(true, node.Start.Line, node.End.Line));
+                        }
+                    }
+                }
+                else
+                {
+                     results.Add(Tuple.Create(false, -1, -1));
+                }
+            }
+
+        }
+        return results;//Tuple.Create(true, 0, 0);
+    }
+
+    
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using YamlDotNet.RepresentationModel;
+using System.Collections.Generic;
+
+namespace YAMLValidator
+{
+    class Program
+    {
+        public static void Main()
+        {
+            // Input yaml file
+            string yamlFile = @"C:\Users\Bilal\Desktop\New folder (2)\openapi.yaml";
+
+            // List of input rules
+            Dictionary<string, Tuple<string, string>> inputRules = new Dictionary<string, Tuple<string, string>>
+            {
+                {"tags.externalDocs.url",Tuple.Create("string", @"^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$")},
+               // {"openapi", Tuple.Create("string", "")},
+               // {"info.title", Tuple.Create("int", "")},
+               // {"info.contact.email", Tuple.Create("string", "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")},
+               // {"servers.url", Tuple.Create("string", @"^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$")}
+            };
+
+            // Parse yaml file
+            var yaml = new YamlStream();
+            using (var reader = new StreamReader(yamlFile))
+            {
+                yaml.Load(reader);
+            }
+
+            // Get the root node
+            var rootNode = yaml.Documents[0].RootNode;
+
+            // Iterate through the input rules and validate against the yaml file
+            foreach (var rule in inputRules)
+            {
+                var tagName = rule.Key;
+                var expectedType = rule.Value.Item1;
+                var regex = rule.Value.Item2;
+                Console.WriteLine("\n tagName:{0}, expectedType :{1}, regex: {2}",tagName, expectedType, regex);
+                var results = GenericRuleEngine.ValidateRule(rootNode, tagName, expectedType, regex);
+                foreach (var result in results)
+                {
+                    if (result.Item1)
+                    {
+                        Console.WriteLine(tagName + " validation succeeded");
+                    }
+                    else
+                    {
+                        Console.WriteLine(tagName + " validation failed at lines " + result.Item2 + " - " + result.Item3);
+                    }
+
+                }
+               
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
